da9e21b6829d8db5fdd9e7cc10eeb08b55fb587e
mattedwards <the.matt.edwards@googlemail.com>
2022-12-10 09:43:42 +0000

    Solved Day10 Part 1

--- day10/day10.js
+++ day10/day10.js
@@ -1,15 +1,19 @@
 const helpers = require("../helpers/helperFunctions");
-const lines = helpers.loadData(__dirname.split("/").pop(), 1);
-console.log(lines);
+const lines = helpers.loadData(__dirname.split("/").pop(), 0);
+// console.log(lines);
 
 let x = 1;
 let c = 0; // cycle
-
+let strengthsSum = 0;
 function cycle(val = null) {
-  console.log(`Cycle ${c + 1} start: x = ${x}`);
+  // console.log(`Cycle ${c + 1} start: x = ${x}`);
+  if ((c + 1 - 20) % 40 === 0) {
+    console.log(`During cycle ${c + 1} signal strength = ${x * (c + 1)}`);
+    strengthsSum += x * (c + 1);
+  }
   if (val) x += val;
   c += 1;
-  console.log(`Cycle ${c} end: x = ${x}`);
+  // console.log(`Cycle ${c} end: x = ${x}`);
 }
 
 while (lines.length > 0) {
@@ -23,3 +27,5 @@ while (lines.length > 0) {
     cycle(parseInt(line.substring(5)));
   }
 }
+
+console.log(`A: Sum of signal strengths = ${strengthsSum}`); //14360
